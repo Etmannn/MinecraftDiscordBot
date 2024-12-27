dab9e3eb600edd75e948aaefcc51912d74b9a2eb
Ethan <ethan.cleminson@gmail.com>
2024-12-27 16:41:41 +0000

    Fixed autostop feature, added some extra informaton on server start, fixed up some grammar and information on config file and made it so you can only call commands from the correct channel.

--- Bot.py
+++ Bot.py
@@ -1,9 +1,8 @@
 # Minecraft Discord Bot by Ethan Cleminson | 10/2/2024
-# Packages used in program
 
+# Packages used in program
 import discord
 from discord.ext import commands, tasks
-import subprocess
 from mcstatus import JavaServer
 import yaml
 import asyncio
@@ -34,11 +33,11 @@ def serverping():
 async def auto_stop():
     online = serverping()
     if online == True and server.status().players.online == 0:
-        await asyncio.sleep(30)
+        await asyncio.sleep(config['time'])
         if online == True and server.status().players.online == 0:
-            subprocess.call("TASKKILL /F /IM java.exe")
+            await asyncio.create_subprocess_shell("TASKKILL /F /IM java.exe")
             if config['playit'] == 'true':
-                subprocess.call("TASKKILL /F /IM playit.exe")
+                await asyncio.create_subprocess_shell("TASKKILL /F /IM playit.exe")
 
 # Prints a message and sets activitiy to the help command; triggers when the bot is online
 @bot.event
@@ -56,76 +55,81 @@ async def on_ready():
 @bot.command()
 @commands.has_role(config['ping'])
 async def ping(ctx):
-    online = serverping()
-    if online is True:
-        await channel.send(f"Server is Online! There are currently {server.status().players.online} players online.")
-    if online is False:
-        await channel.send("Server is Offline")
+    if ctx.channel.id == config['channel']:
+        online = serverping()
+        if online is True:
+            await channel.send(f"Server is Online! There are currently {server.status().players.online} players online.")
+        if online is False:
+            await channel.send("Server is Offline")
 
 # A command to start the Minecraft server
 # If the server is already online it will inform the user
 @bot.command()
 @commands.has_role(config['start'])
 async def start(ctx):
-    online = serverping()
-    if online is True:
-        await channel.send("Server already Online")
-    else:
-        await ctx.send("Server is starting!")
-        subprocess.Popen(f"start cmd.exe /c {config['startbat']}", shell=True)
-        if config['playit'] == 'true':
-            subprocess.call("TASKKILL /F /IM playit.exe")
-            subprocess.Popen('start cmd.exe /c playit.exe', shell=True)
-        checking = True
-        while checking is True:
-            checkstat = serverping()
-            if checkstat is True:
-                await channel.send("Server Online!")
-                checking = False
+    if ctx.channel.id == config['channel']:
+        online = serverping()
+        if online is True:
+            await channel.send("Server already Online")
+        else:
+            await ctx.send("Server is starting!")
+            await asyncio.create_subprocess_shell(f"start cmd.exe /c {config['startbat']}", shell=True)
+            if config['playit'] == 'true':
+                await asyncio.create_subprocess_shell("TASKKILL /F /IM playit.exe")
+                await asyncio.create_subprocess_shell('start cmd.exe /c playit.exe', shell=True)
+            checking = True
+            while checking is True:
+                checkstat = serverping()
+                if checkstat is True:
+                    await channel.send(f"Server Online! The server will automatically stop in {config['time']} seconds.")
+                    checking = False
 
 # A command to stop the Minecraft server
 # If the server is not online it will inform the user
 @bot.command()
 @commands.has_role(config['stop'])
 async def stop(ctx):
-    online = serverping()
-    if online is True:
-        subprocess.call("TASKKILL /F /IM java.exe")
-        if config['playit'] == 'true':
-            subprocess.call("TASKKILL /F /IM playit.exe")
-        await channel.send("Server Offline")
-    else:
-        await channel.send("Sever already Offline")
+    if ctx.channel.id == config['channel']:
+        online = serverping()
+        if online is True:
+            await asyncio.create_subprocess_shell("TASKKILL /F /IM java.exe")
+            if config['playit'] == 'true':
+                await asyncio.create_subprocess_shell("TASKKILL /F /IM playit.exe")
+            await channel.send("Server Offline")
+        else:
+            await channel.send("Sever already Offline")
 
 # A command to restart the Minecraft server
 @bot.command()
 @commands.has_role(config['restart'])
 async def restart(ctx):
-    online = serverping()
-    if online is True:
-        await stop(ctx)
-        await start(ctx)
-    else:
-        await channel.send("Server Offline")
+    if ctx.channel.id == config['channel']:
+        online = serverping()
+        if online is True:
+            await stop(ctx)
+            await start(ctx)
+        else:
+            await channel.send("Server Offline")
 
 
 # Displays all the commands of the bot in a discord embed
 @bot.command()
 @commands.has_role(config['help'])
 async def help(ctx):
-    helpembed = discord.Embed(title="Commands", color=0x55FF55)
+    if ctx.channel.id == config['channel']:
+        helpembed = discord.Embed(title="Commands", color=0x55FF55)
 
-    helpembed.add_field(name=f"{config['prefix']}ping", value="Checks Server Status", inline=False)
-    helpembed.add_field(
-        name=f"{config['prefix']}start", value="Starts the Terraria server", inline=False
-    )
-    helpembed.add_field(
-        name=f"{config['prefix']}stop", value="Stops the Terraria server", inline=False
-    )
-    helpembed.add_field(
-        name=f"{config['prefix']}restart", value="Restarts the Terraria server", inline=False
-    )
-    await ctx.send(embed=helpembed)
+        helpembed.add_field(name=f"{config['prefix']}ping", value="Checks Server Status", inline=False)
+        helpembed.add_field(
+            name=f"{config['prefix']}start", value="Starts the Terraria server", inline=False
+        )
+        helpembed.add_field(
+            name=f"{config['prefix']}stop", value="Stops the Terraria server", inline=False
+        )
+        helpembed.add_field(
+            name=f"{config['prefix']}restart", value="Restarts the Terraria server", inline=False
+        )
+        await ctx.send(embed=helpembed)
 
 # Runs the bot using the bot token
 bot.run(config['token'])
